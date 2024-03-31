50b3783ac401fdd85e752d8b5ec584ee20de8f37
gabriel-tomas <gabriel.tomas.com@gmail.com>
2024-03-31 12:19:29 -0300

    fix git

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -1,14 +1,25 @@
 /* import { useEffect, useState } from 'react'; */
 import EditorJS from '@editorjs/editorjs';
 import Header from '@editorjs/header';
+import { useEffect, useState } from 'react';
 
 function App() {
-  new EditorJS({
-    holder: 'editor',
-    tools: {
-      header: Header,
-    },
-  });
+  const [editor, setEditor] = useState<EditorJS | null>(null);
+
+  useEffect(() => {
+    const editorJs = new EditorJS({
+      holder: 'editor',
+      tools: {
+        header: Header,
+      },
+    });
+
+    setEditor(editorJs);
+
+    return () => {
+      editor && editor.destroy();
+    };
+  }, []);
 
   return <div id="editor"></div>;
 }
